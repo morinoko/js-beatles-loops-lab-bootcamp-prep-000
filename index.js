a72d03b8bd4c8769a4348308c8eff31d08ec7051
Felice Forby <feliceforby@gmail.com>
2017-12-28 13:45:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function johnLennonFacts(facts) {
   var array = [];
   let i = 0;
   while (i < facts.length ) {
-    array.push(facts[i++])
+    array.push(facts[i++] + "!!!");
   }
+  return array;
 }
