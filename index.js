fdaba7cf64ae38fe649b701ce199dbc6038d8a9f
Felice Forby <feliceforby@gmail.com>
2017-12-28 13:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatles = [];
   for (i = 0; i <= 4; i++) {
-    array.push("$musicians[i] plays ${")
+    array.push("${musicians[i]} plays ${instruments{i}");
   }
+  return beatles;
 }
